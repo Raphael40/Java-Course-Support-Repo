08358894e71620a75fc2beb9688aad57453e49df
Julian Festing <julfesting@gmail.com>
2024-06-04 15:12:14 +0100

    finish FirstClass and add DataStructures

---
+++ exercises/FirstClass.java
@@ -0,0 +1,24 @@
+package exercises;
+
+public class FirstClass {
+
+    public static void main(String[] items) {
+        String text = "This is my first class";
+        System.out.println(text);
+
+        int ten = 10;
+        int three = 3;
+
+        System.out.println(ten * three);
+
+        System.out.println(++ten);
+
+        System.out.println(three > ten);
+
+        System.out.println((three < 20 && ten < 20));
+
+        System.out.println(!true);
+    }
+
+}
+
