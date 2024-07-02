e672c9a480cd05b543e2829756c76e072261bcd4
JFesting <julfesting@gmail.com>
2024-07-02 22:38:38 +0100

    file restructure

--- main/java/HelloWorld.java
+++ main/java/HelloWorld.java
@@ -1,4 +1,4 @@
-package exercises;
+package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- main/java/classes/FirstClass.java
+++ main/java/classes/FirstClass.java
@@ -1,4 +1,4 @@
-package exercises;
+package main.java.classes;
 
 public class FirstClass {
 
