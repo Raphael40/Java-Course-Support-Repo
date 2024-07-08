e1cc9c5206274a8609d31cc76e5cf3b9607d03bf
JFesting <julfesting@gmail.com>
2024-07-08 20:50:54 +0100

    setup starter

--- src/main/java/loops/Iterators.java
+++ src/main/java/loops/Iterators.java
@@ -2,26 +2,6 @@ package main.java.loops;
 
 public class Iterators {
     public static void main(String[] args) {
-        for (int counter = 1; counter <= 10; counter++) {
-            System.out.print(counter);
-        }
 
-        for (int counter = 10; counter >= 1; counter--) {
-            System.out.println("launching in " + counter);
-        }
-        System.out.println("Blast off!");
-
-        int i = 0;
-
-        while (i <= 5) {
-            System.out.println(i);
-            i++;
-        }
-
-        for (int counter = 1; counter <= 10; counter++) {
-            if (counter % 2 == 1) {
-                System.out.print(counter);
-            }
-        }
     }
 }
--- src/main/java/variables_and_operators/Variables.java
+++ src/main/java/variables_and_operators/Variables.java
@@ -2,17 +2,7 @@ package main.java.variables_and_operators;
 
 public class Variables {
     public static void main (String[] args) {
-        int myNumber = 10;
-        double myDouble = 2.5;
-        char myLetter = 'B';
-        boolean myBoolean = true;
-        String myString = "Kitten";
 
-        System.out.println(myNumber);
-        System.out.println(myDouble);
-        System.out.println(myLetter);
-        System.out.println(myBoolean);
-        System.out.println(myString);
     }
 
 }
